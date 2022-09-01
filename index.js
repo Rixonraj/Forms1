b90a7ae95347d10194d039f498bbf1ac83ec5632
Rixonraj <85862632+Rixonraj@users.noreply.github.com>
2022-09-01 15:03:41 +0530

    Add files via upload
    Forms

---
+++ index.js
@@ -0,0 +1,7 @@
+function consoleFunc(){
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const middleName = document.getElementById("middleName").value;
+    const email = document.getElementById("email").value;
+    console.log(firstName,lastName,middleName,email)
+    }
